5a692a808ff32a48ca4cd41f553b60fd11aa558a
Oltion-Koceku <koceku17@gmail.com>
2024-02-29 10:35:55 +0100

    [fix]palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,11 +2,14 @@
 const parolaUtente = prompt("Inserire una parola")
 const parolagirata = palidroma(parolaUtente);
 console.log(parolagirata);
+const output = document.querySelector("h1");
 
 if(ePalindroma(parolaUtente, parolagirata)){
   console.log("la parola è palindroma");
+  output.innerHTML += `la parola ${parolaUtente} è palindroma`
 }else{
   console.log("la parola non è palindroma");
+  output.innerHTML += `la parola ${parolaUtente} non è palindroma`
 }
 
 function palidroma(parolaDagirtare) {
